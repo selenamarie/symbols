64c773730e019a6ca1d32d0e54d1f5da8d49a883
Selena Deckelmann <selenamarie@gmail.com>
2013-02-06 14:20:32 -0800

    Input module id into database

--- symbols.py
+++ symbols.py
@@ -9,6 +9,29 @@ class Symbol():
     def __init__(self):
         self.symboldb = SymbolDB()
 
+    def _add_module(self, m):
+        try:
+            new_module = Module(os=m.group(1), arch=m.group(2), hexid=m.group(3), name=m.group(4))
+            self.symboldb.session.add(new_module)
+            self.symboldb.session.commit()
+        except:
+            return None
+
+        return(new_module.id)
+
+    def _add_file(self, m, mod_id):
+        try:
+            new = File(number=m.group(1), name=m.group(2), module=mod_id)
+            self.symboldb.session.add(new)
+            self.symboldb.session.commit()
+        except:
+            return None
+
+        return(new.id)
+
+
+    def add(self, url):
+
     def add(self, url):
         page = urllib.urlopen(url)
         symbols = page.read().split('\n')
@@ -19,9 +42,11 @@ class Symbol():
             # MODULE windows x86 E40644FDC4D040749962D4C8EB8DF3212 firefox.pdb
             m = re.search('^MODULE (\S+) (\S+) (\S+) (\S+)', line)
             if m:
-                new_module = Module(os=m.group(1), arch=m.group(2), hexid=m.group(3), name=m.group(4))
-                self.symboldb.session.add(new_module)
-                self.symboldb.session.commit()
+                mod_id = self._add_module(m)
+                next
+            m = re.search('^FILE (\S+) (\S+)', line)
+            if m:
+                file_id = self._add_file(m, mod_id)
 
 if __name__ == "__main__":
     test = Symbol()
